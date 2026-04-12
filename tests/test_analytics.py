f0798a0ddc14f038099e8d4846d65696d5028b2a
Mariyam Zaidi <mariyamzaidi1503@gmail.com>
2026-04-12 17:40:37 +0200

    Adding more tests

--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -4,3 +4,11 @@ def test_expired_count():
 
     inv=Inventory("data/my_actual_alseer_portfolio.csv")
     assert inv.metrics['expired_count'] > 0
+
+def test_urgent_brands():
+    inv = Inventory("data/my_actual_alseer_portfolio.csv")
+    assert len(inv.metrics['urgent_brands']) > 0
+
+def test_total_inventory_value():
+    inv = Inventory("data/my_actual_alseer_portfolio.csv")
+    assert inv.metrics['total_inv_val'] > 0
